f9b935e10f503cb202fbbed4e8684cbde9259fd2
Beatriz Herranz <167246883+BeatrizHerranz@users.noreply.github.com>
2024-11-25 18:05:11 +0000

    navbar, footer y home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+export const Navbar = ({ store }) => {
+
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<nav className="navbar navbar-expand-lg navbar-light fixed-top">
+			<div className="container-fluid">
+				<Link className="navbar-brand" to="/">
+					CreArte
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" to="/categories">Clases mensuales</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" to="/courses">Bonos regalo</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link" to="/courses">Contacto</Link>
+						</li>
+					</ul>
+
+					<Link to="/cart" className="btn btn-outline-dark cart-button">
+						<i className="fas fa-shopping-cart"></i>
+						<span className="badge bg-secondary">
+							{store.cartItems.length > 0 ? store.cartItems.length : 0}
+						</span>
 					</Link>
+
+					<Link to="/login" className="btn btn-outline-primary ms-2">Iniciar Sesión</Link>
+					<Link to="/register" className="btn btn-outline-secondary ms-2">Registrarse</Link>
 				</div>
 			</div>
 		</nav>
-	);
+  );
 };
+
+export default Navbar;

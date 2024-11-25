4168cb9e760f1becc28798289027066e7d6f8e4e
Beatriz Herranz <167246883+BeatrizHerranz@users.noreply.github.com>
2024-11-25 18:05:11 +0000

    navbar, footer y home

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<div className="footer-content">
+				<div className="contact-info">
+					<h5>CreArte</h5>
+					<p>Correo: hola@crearte.es</p>
+					<p>Teléfono: +34 000 000 000</p>
+				</div>
+
+				<div className="social-links">
+					<ul>
+						<li><a href="#" className="facebook">Facebook</a></li>
+						<li><a href="#" className="instagram">Instagram</a></li>
+						<li><a href="#" className="twitter">Twitter</a></li>
+					</ul>
+				</div>
+
+				<div className="legal-info">
+					<p>© 2024 CreArte. Todos los derechos reservados.</p>
+					<Link to="/politica-de-cookies">Política de cookies</Link>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
